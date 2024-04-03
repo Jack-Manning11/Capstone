c09f485ac03f3b5911496b89e95afad7e2d8d185
jagreer7 <jagr4452@colorado.edu>
2024-04-02 20:06:52 -0600

    Added a special tripwire for the top of the elevator ride

--- Assets/Scripts/TripwireDialogue.cs
+++ Assets/Scripts/TripwireDialogue.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class TripwireDialogue : MonoBehaviour
 {
     private int numberOfConvo = 0;
+    [SerializeField] bool canBeRemoved;
+
+    public DialogueBoxSender dialogueBoxSender;
+    public DialougeQuiz dialogueQuiz;
+    public GameObject ObjectToDestroy;
 
     private void OnTriggerEnter2D(Collider2D other) //When the player enters the dialogue box
     {
@@ -12,6 +17,13 @@ public class TripwireDialogue : MonoBehaviour
         if (numberOfConvo == 1)
         {
             this.GetComponent<DialogueBoxSender>().TriggerDialogue();
+        }
+    }
+    private void Update()
+    {
+        if (canBeRemoved && dialogueQuiz.senderCopy.nameOfCharacter == dialogueBoxSender.nameOfCharacter && dialogueBoxSender.SuccessfulQuiz == true)
+        {
+            ObjectToDestroy.SetActive(false);
         }
     }
 }
